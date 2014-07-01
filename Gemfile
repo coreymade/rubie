4351173c39b75764948f499c312b3af75680afd5
Corey Haggard <corey@gofullstack.com>
2014-06-30 20:32:43 -0700

    Adding new structure with haml

--- Gemfile
+++ Gemfile
@@ -2,14 +2,13 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~>3.3.3"
+gem "middleman", "~>3.2.2"
 gem 'middleman-gh-pages'
+gem 'bourbon'
+gem 'neat'
 
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
-
-# Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
@@ -36,9 +36,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+# activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -55,6 +53,8 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+  activate :relative_assets
+  set :relative_links, true
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
